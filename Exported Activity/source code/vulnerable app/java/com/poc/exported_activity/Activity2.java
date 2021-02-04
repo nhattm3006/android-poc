c10511c468e3b9c082947de3ec78b9f9ae54ed5d
Tran Minh Nhat <trminhnhat99@gmail.com>
2021-02-04 13:04:37 +0700

    Upload vulnerable app's source code

---
+++ Exported Activity/source code/vulnerable app/java/com/poc/exported_activity/Activity2.java
@@ -0,0 +1,19 @@
+package com.poc.exported_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        textView = findViewById(R.id.textview);
+    }
+}
---
+++ Exported Activity/source code/vulnerable app/java/com/poc/exported_activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.poc.exported_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
